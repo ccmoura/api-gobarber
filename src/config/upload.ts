94fb5e06f5e92dc5d8526437d6f9212653687e32
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2020-07-30 22:10:18 -0300

    DDD

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,7 @@ const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 export default {
     directory: tmpFolder,
     storage: multer.diskStorage({
-        destination: ,
+        destination: tmpFolder,
         filename: (request, file, callback) => {
             const fileHash = crypto.randomBytes(10).toString('HEX');
             const fileName = `${fileHash}-${file.originalname}`;
