3f88dc840fc3a5580835afcc898a070891186e1d
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2020-08-16 15:32:28 -0300

    tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,13 +2,16 @@ import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from './CreateAppointmentService';
 
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointmentsService;
 describe('CreateAppointment', () => {
-    it('should be able to create a new appointment', async () => {
-        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointment = new CreateAppointmentsService(
+    beforeEach(() => {
+        fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        createAppointment = new CreateAppointmentsService(
             fakeAppointmentsRepository,
         );
-
+    });
+    it('should be able to create a new appointment', async () => {
         const appointment = await createAppointment.execute({
             date: new Date(),
             provider_id: '0523146',
@@ -19,11 +22,6 @@ describe('CreateAppointment', () => {
     });
 
     it('should not to be able to create two appointments on the same time', async () => {
-        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-        const createAppointment = new CreateAppointmentsService(
-            fakeAppointmentsRepository,
-        );
-
         const appointmentDate = new Date(2020, 4, 10, 11);
 
         await createAppointment.execute({
--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,21 @@
 import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
+let fakeHashProvider: FakeHashProvider;
+let fakeUsersRepository: FakeUsersRepository;
+let createUser: CreateUserService;
 describe('CreateUser', () => {
+    beforeEach(() => {
+        fakeHashProvider = new FakeHashProvider();
+        fakeUsersRepository = new FakeUsersRepository();
+        createUser = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+    });
     it('should be able to create a new user', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const createUser = new CreateUserService(fakeUsersRepository);
-
         const user = await createUser.execute({
             name: 'Christopher Moura',
             email: 'teste@example.com',
@@ -17,16 +26,13 @@ describe('CreateUser', () => {
     });
 
     it('should not be able to create a new user with same email from another', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const createUser = new CreateUserService(fakeUsersRepository);
-
         await createUser.execute({
             name: 'Christopher Moura',
             email: 'teste@example.com',
             password: '123456',
         });
 
-        expect(
+        await expect(
             createUser.execute({
                 name: 'Christopher Moura',
                 email: 'teste@example.com',
